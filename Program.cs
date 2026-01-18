9eba62eaa951983aced990ab5b3ba403a9ce5963
Savija Fernando <savijanethika@gmail.com>
2026-01-18 20:16:44 +0530

    Difference of WriteLine and Write.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, Savija!");
-Console.Write("Congragulations, ");
-Console.Write(" ");
+//Console.Write("Congragulations, ");
+//Console.Write(" ");
+//Console.Write("You wrote your first lines of code.");
+Console.WriteLine("Congratulations!");
 Console.Write("You wrote your first lines of code.");
