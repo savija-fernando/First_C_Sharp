759358e1517a8301180ca8ae9fab3f0e69d4e806
Savija Fernando <savijanethika@gmail.com>
2026-01-18 14:08:37 +0530

    Initial C# console app

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, Savija!");
+Console.Write("Congragulations, ");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
