f187a7a7fb290d79a3be9bdbaaf7dc8bfff9ae14
Savija Fernando <savijanethika@gmail.com>
2026-01-19 20:57:04 +0530

    Display literal and variable values

--- Program.cs
+++ Program.cs
@@ -9,15 +9,28 @@
 //Console.Write("This is the second line");
 
 //Use character literals
-Console.WriteLine('H');
+//Console.WriteLine('H');
 
 //Use integer literals
-Console.WriteLine(100);
+//Console.WriteLine(100);
 
 //Use floating-point literals
-Console.WriteLine(99.99F);
-Console.WriteLine(12.39816m);
+//Console.WriteLine(99.99F);
+//Console.WriteLine(12.39816m);
 
 //Use Boolean literals
-Console.WriteLine(true);
-Console.WriteLine(false);
+//Console.WriteLine(true);
+//Console.WriteLine(false);
+
+//Declare a variable
+//string firstName="Savija";
+//or firstName = "Savija";
+//Console.WriteLine(firstName);
+//char userOption;
+//int gameScore;
+
+//Declare implicitly typed local variables
+string name="Bob";
+var value=3;
+decimal temprerature=34.4m;
+Console.WriteLine($"Hello, {name}! You have {value} messages in your inbox. The Temperature is {temprerature} celcius.");
