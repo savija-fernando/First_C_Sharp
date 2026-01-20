592115b05e95898262e46913a0a302a9a568ee35
Savija Fernando <savijanethika@gmail.com>
2026-01-20 19:27:23 +0530

    character escape sequences

--- Program.cs
+++ Program.cs
@@ -8,29 +8,43 @@
 //Console.WriteLine("This is the first line");
 //Console.Write("This is the second line");
 
-//Use character literals
+//1.0Use character literals
 //Console.WriteLine('H');
 
-//Use integer literals
+//2.0Use integer literals
 //Console.WriteLine(100);
 
-//Use floating-point literals
+//3.0Use floating-point literals
 //Console.WriteLine(99.99F);
 //Console.WriteLine(12.39816m);
 
-//Use Boolean literals
+//4.0Use Boolean literals
 //Console.WriteLine(true);
 //Console.WriteLine(false);
 
-//Declare a variable
+//5.0Declare a variable
 //string firstName="Savija";
 //or firstName = "Savija";
 //Console.WriteLine(firstName);
 //char userOption;
 //int gameScore;
 
-//Declare implicitly typed local variables
-string name="Bob";
-var value=3;
-decimal temprerature=34.4m;
-Console.WriteLine($"Hello, {name}! You have {value} messages in your inbox. The Temperature is {temprerature} celcius.");
+//6.0Declare implicitly typed local variables
+
+//string name="Bob";
+//var value=3;
+//decimal temprerature=34.4m;
+//Console.WriteLine($"Hello, {name}! You have {value} messages in your inbox. The Temperature is {temprerature} celcius.");
+ 
+ //7.0Format literal strings in C#
+ //7.1Character escape sequences
+ //Console.WriteLine("Hello,\nSavija!");
+ //Console.WriteLine("Hello,\tSavija!");
+ //7.3display a file path?
+ //Console.WriteLine("C:\\Program Files\\MyApp\\app.exe");
+
+ Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\tComplete!");
+Console.WriteLine("Invoice: 1022\tComplete!");
+Console.WriteLine("\nOutput Directory:");
+
