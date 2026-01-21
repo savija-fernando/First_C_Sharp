090016a30f8f21d1e05477cc9fb1b9ed13011ffa
Savija Fernando <savijanethika@gmail.com>
2026-01-21 20:16:42 +0530

    Perform addition with implicit data conversion

--- Program.cs
+++ Program.cs
@@ -59,9 +59,20 @@
 //challenge
 using System.IO.Compression;
 
-string projectName = "ACME";
-string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
-Console.WriteLine($@"View English output:
-        c:\Exercise\{projectName}\data.txt");
-Console.WriteLine($@"{russianMessage}:
-        c:\Exercise\{projectName}\data.txt");
+//string projectName = "ACME";
+//string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+//Console.WriteLine($@"View English output:
+  //      c:\Exercise\{projectName}\data.txt");
+//Console.WriteLine($@"{russianMessage}:
+  //      c:\Exercise\{projectName}\data.txt");
+
+//directory = directory + @"\";
+
+//Add two numeric values
+//int firstNumber = 12;
+//int secondNumber = 7;
+//Console.WriteLine(firstNumber+secondNumber);
+
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName+" sold "+widgetsSold+ 7 +" widgets.");
